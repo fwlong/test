a6fb28da1da6e7a5fc2071e7731d9950e0b7013e
seph_von <fwlong2008@126.com>
2018-02-06 21:47:57 +0800

    导航栏完善

--- app/src/main/java/cn/jsjst/reimbursement/base/BaseActivity.java
+++ app/src/main/java/cn/jsjst/reimbursement/base/BaseActivity.java
@@ -1,12 +1,8 @@
 package cn.jsjst.reimbursement.base;
 
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-
-import cn.jsjst.reimbursement.R;
-import cn.jsjst.reimbursement.databinding.ActivityTestBinding;
 
 /**
  * 类说明
@@ -21,38 +17,7 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityTestBinding atb = DataBindingUtil.setContentView(this, R.layout.activity_test);
-        atb.setBaseModel(new BaseViewModel(this) {
-            @Override
-            public void showLoading() {
-                super.showLoading();
-            }
 
-            @Override
-            public void finishLoading() {
-                super.finishLoading();
-            }
-
-            @Override
-            public void onCreate() {
-                super.onCreate();
-            }
-
-            @Override
-            public void onStart() {
-                super.onStart();
-            }
-
-            @Override
-            public void onStop() {
-                super.onStop();
-            }
-
-            @Override
-            public void onDestory() {
-                super.onDestory();
-            }
-        });
     }
 }
 
