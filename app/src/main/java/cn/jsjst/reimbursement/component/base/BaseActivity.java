b4f204f611765ffcee9b4328f3ac66522e5137b1
fengwenlong <wenlong.feng@mi-me.com>
2018-03-01 10:44:59 +0800

    颜色调整

--- app/src/main/java/cn/jsjst/reimbursement/component/base/BaseActivity.java
+++ app/src/main/java/cn/jsjst/reimbursement/component/base/BaseActivity.java
@@ -1,9 +1,13 @@
 package cn.jsjst.reimbursement.component.base;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
+
+import cn.jsjst.reimbursement.R;
 
 /**
  * 类说明
@@ -18,6 +22,10 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            getWindow().setStatusBarColor(getColor(R.color.color_white));
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
     }
 
     public void refreshView(View view){}
